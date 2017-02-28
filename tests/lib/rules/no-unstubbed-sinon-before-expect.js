449ad8d2e62142ede853f327b823ce9ad1480048
Evan Lloyd <elloyd@patientslikeme.com>
2017-02-28 16:33:00 -0500

    make a big mess

--- tests/lib/rules/no-unstubbed-sinon-before-expect.js
+++ tests/lib/rules/no-unstubbed-sinon-before-expect.js
@@ -21,16 +21,26 @@ var ruleTester = new RuleTester();
 ruleTester.run("no-unstubbed-sinon-before-expect", rule, {
 
   valid: [
-    `const ajaxStub = sinon.stub(AjaxHelpers, 'post', ajaxCallBack);
-       ajaxStub.restore();
-       expect(true).toEqual('cat');`,
+    // {
+    //   code:`it("passes shit", () => {
+    //     var ajaxStub = sinon.stub(AjaxHelpers, 'post', ajaxCallBack);
+    //     ajaxStub.restore();
+    //     expect(true).toEqual('cat');
+    //   });`,
+    //   globals: ['it'],
+    //   parserOptions: { ecmaVersion: 6  },
+    // },
   ],
 
   invalid: [
     {
-      code: `const ajaxStub = sinon.stub(AjaxHelpers, 'post', ajaxCallBack);
-            expect(true).toEqual('cat');
-            ajaxStub.restore();`,
+      code:`it("should fail", () => {
+        var ajaxStub = sinon.stub(AjaxHelpers, 'post', ajaxCallBack);
+        expect(true).toEqual('cat');
+        ajaxStub.restore();
+      });`,
+      parserOptions: { ecmaVersion: 6  },
+      globals: ['it'],
       errors: [{
         message: "Restore stubs before `expect`",
         type: "Otherwise terrible things will happen"
